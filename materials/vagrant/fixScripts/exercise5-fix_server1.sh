e94802fad704f1e11311eb9776fd540fe2130b74
RYair <ryair@itamar-medical.com>
2021-12-09 14:49:19 +0200

    finish all exercise

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,93 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+
+echo "-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn
+NhAAAAAwEAAQAAAYEA2uLf0+35obHluswQCOh8qnEZbkrEHnbIKOUzFo8eRDRbePt1RhKF
+TvH9G1F0bx3WiufhmbKKMeu41GH5zulULmUXlmW8jCJKKhZp1p6QT5hOOgO5YmEHzNZsq2
+DjKOr0TGdoW1isntXKKOJzLNgeaz8mR8qSPnNAF8KbRn9Lu5u+BRaX8FMwPDvoZDx6LGPw
+f2z1eqT54SRgRoFI2Q+krvDoPqsiZpKYPPIWbolRRaCCeO2l6kyhDCatPdV0HjA0nF9dKQ
+vkFiKCzwY7P1CRlCrk5gaucZ31KbiLjSibkCOs222MCqDbNoTe0rJjQxf3bRAlcDMUP8p9
+wK3jV7uqhTusf31ubE00gbTkqWRIoTaUlT0OHf0AMrZHVADB12CLLz+a3KEaHEg6PjOwpK
+dpnPzyGcmLKpjgDeCydnOieTIgDS7LY9EMdZaqS6pDg8+LG3LQ4fEv1Mim4HJTDNTxU/mB
+2O9cn+aDIJcKHu246xrmfgOr/qEh9IVnI+nDQJo9AAAFiHz5iJR8+YiUAAAAB3NzaC1yc2
+EAAAGBANri39Pt+aGx5brMEAjofKpxGW5KxB52yCjlMxaPHkQ0W3j7dUYShU7x/RtRdG8d
+1orn4ZmyijHruNRh+c7pVC5lF5ZlvIwiSioWadaekE+YTjoDuWJhB8zWbKtg4yjq9ExnaF
+tYrJ7VyijicyzYHms/JkfKkj5zQBfCm0Z/S7ubvgUWl/BTMDw76GQ8eixj8H9s9Xqk+eEk
+YEaBSNkPpK7w6D6rImaSmDzyFm6JUUWggnjtpepMoQwmrT3VdB4wNJxfXSkL5BYigs8GOz
+9QkZQq5OYGrnGd9Sm4i40om5AjrNttjAqg2zaE3tKyY0MX920QJXAzFD/KfcCt41e7qoU7
+rH99bmxNNIG05KlkSKE2lJU9Dh39ADK2R1QAwddgiy8/mtyhGhxIOj4zsKSnaZz88hnJiy
+qY4A3gsnZzonkyIA0uy2PRDHWWqkuqQ4PPixty0OHxL9TIpuByUwzU8VP5gdjvXJ/mgyCX
+Ch7tuOsa5n4Dq/6hIfSFZyPpw0CaPQAAAAMBAAEAAAGAKsD/2UR1axj1d2wega7vktcukX
+V3I0AuTpaeSPzBe68CYvoy6PH7KfVoa+qYJJAedNlgHoZaKfASFSHiCoFW2Q0T82OB1Zlp
+BRmFqM8l/vYP6ZMKpeWwAVmstjjhhf+Golncb2M9rHSz3RKGTq8UGfymvIUb1ajrTvjLHZ
+UFtOpxzkqgeOe21z0YYmPRtVP8CNe+hyssR8a0K3FbSlKc5zLVGlCREpn1tInQydx2jHQS
+qA47Ry2ak2YGlrpXyQG/GIaQd1Q1EbcSGGjPhDcGS2uGFHnLF1JkiIcR/FFPLXJFFxLSur
+x661JY6Y4lQ6gP/XTUni0iGXtKYtarFdoj1WykhIg/dbMBzZ0M3fShc68loDWgxcJ4wpV/
+bwdH71f5ra/Xe6WK3euaHMi/Jx8ngbVq5++DwgjbZrFoOrEibejUNxD9OVpoQoRd90ucJL
+VG22+DmEe2vRZTlmdh97WQydRQVGP7v4DDSGCGRnx9JWB5651ouCnbrz7V6KjvS239AAAA
+wES2qmtwLJz50iYhni/wsOyh083ZAZWpQM5hnLqXcShIevFryXyEiZ3S2wPfsqLTCkmz69
+EDyBO9JptzofNy3/3QCRk5t/9fbWR2pTKd7e5HNvkhbXabOE+ef+5RV2IcM28MGnialNI4
+DoQ5hIkb3buYyP936Fg4d0H7vsIMYwxemfl9Z5lI0Vii1TxE2VEoXD2Na3odEs8c0mcrfb
+2zfe4RvEdGXYWugCWBFVSDLDagQ3uD0tEslQfXWG9Qnlpv2AAAAMEA7SfVq5LZwsVSF+NP
+zTecnlobxfAIusiwkPMSlRIXYQXaTyJoZLwrmDADdaOp+oZd2bgDRmaYN66k5JF4fcYeue
+ceTvpxN2AYa/zROSce4HifiFb2xaX6T14EFWXDBLHyhkNAZpU2EePVyhYIPHLgTIuhb1B6
+hkFtH7xVjc9gBHOPy9x0+ocJ6TQSNFs3ylWE8dYicIrRoJsFziLnRq0KVhAEaLNPySAxaM
+OJAVu1duIdgUesY6lAG2hLG4xgd+6fAAAAwQDsR2h+o6Ynr0SGM71opbxV86IHRd9OsQif
+f7AOma0bEbFS4TYiqMwuIolAeoTZ/aiVgff8hvwCZsOPE3CKYYEA+gDv4aRQkyBunXLKdk
+Af9m02wB1RHZjbABG3yBzUAPMPTq78GHCFNpNIPfoA2N74ZBqVRCKhK2dXE+LBng7CWmip
+DKg88J/q57vqPaNtIlwPxEH5vjiCwDRJEQnXAP7SdBNz3jb3SCxg0RyLC+lMePRDFRCXsm
+5BS5AS3foLdaMAAAAPdmFncmFudEBzZXJ2ZXIxAQIDBA==
+-----END OPENSSH PRIVATE KEY-----" > /home/vagrant/.ssh/id_rsa
+
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDa4t/T7fmhseW6zBAI6HyqcRluSsQedsgo5TMWjx5ENFt4+3VGEoVO8f0bUXRvHdaK5+GZsoox67jUYfnO6VQuZReWZbyMIkoqFmnWnpBPmE46A7liYQfM1myrYOMo6vRMZ2hbWKye1coo4nMs2B5rPyZHypI+c0AXwptGf0u7m74FFpfwUzA8O+hkPHosY/B/bPV6pPnhJGBGgUjZD6Su8Og+qyJmkpg88hZuiVFFoIJ47aXqTKEMJq091XQeMDScX10pC+QWIoLPBjs/UJGUKuTmBq5xnfUpuIuNKJuQI6zbbYwKoNs2hN7SsmNDF/dtECVwMxQ/yn3AreNXu6qFO6x/fW5sTTSBtOSpZEihNpSVPQ4d/QAytkdUAMHXYIsvP5rcoRocSDo+M7Ckp2mc/PIZyYsqmOAN4LJ2c6J5MiANLstj0Qx1lqpLqkODz4sbctDh8S/UyKbgclMM1PFT+YHY71yf5oMglwoe7bjrGuZ+A6v+oSH0hWcj6cNAmj0= vagrant@server1"> /home/vagrant/.ssh/id_rsa.pub
+
+cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+
+echo "Host server2
+        StrictHostKeyChecking no" > /etc/ssh/ssh_config
+
+sudo service ssh restart
+
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa.pub
+chmod 600 /home/vagrant/.ssh/id_rsa 
+
+
+
+
+
+    
+# mkdir -p /tmp/waiter
+# touch /tmp/waiter/waiter.log
+# chmod a+w /tmp/waiter/waiter.log
+# # crontab -e 
+# sudo cat <<EOF > /home/vagrant/waiter.sh
+
+# #!/bin/bash
+# sudo chmod +x /home/vagrant/waiter.sh
+
+# while ! nc -z 192.168.100.11 22; do
+#         echo "sleep for 1s" >> /tmp/waiter/waiter.log && sleep 1
+# done
+# echo "server is up, copying the public key to the other server" >> /tmp/waiter/waiter.log
+# sleep 20
+# echo "done WAITING " >> /tmp/waiter/waiter.log
+
+# sshpass -p "vagrant" ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub 192.168.100.11
+
+# echo "done script" >> /tmp/waiter/waiter.log
+# EOF
+
+# sudo chmod +x /home/vagrant/waiter.sh
+
+
+# # sudo crontab -l > cron_bkp
+# # sudo echo "@reboot vagrant /home/vagrant/waiter.sh" >> /etc/crontab
+# # sudo crontab cron_bkp
+# # sudo rm cron_bkp
+
+# # (crontab -l 2>/dev/null; echo "1 * * * * vagrant /home/vagrant/waiter.sh") | crontab -
+
+# su - vagrant crontab -l > file; echo "1 * * * *  sleep 120; /home/vagrant/waiter.sh" >> file; crontab file; rm file
+# # (crontab -l -u vagrant 2>/dev/null ; echo "@reboot /home/vagrant/waiter.sh") | crontab -    ב
--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,67 @@
-#!/bin/bash
+# !/bin/bash
 #add fix to exercise6-fix here
+
+# Steps:
+#  1. Check where you sould copy the files
+#  2. Check argument are valide: 
+        # at least 2 args 
+        # all files exists  
+        # dirctory is exsists -> if not create it 
+#  3. Copy the files....
+
+check_remote_dir() {
+        printf "\ntesting remote directory: '$1' \n"
+        # $1 = Server name  , $2 = directory name 
+        if ssh "$1" "[ ! -d $2 ]"; then
+                printf "\nCreating: $1 on $1\n"
+                ssh "$1" "mkdir -p $2"
+        else
+                printf "[OK]\n"
+        fi
+}
+
+set_destantion(){
+    hs=$HOSTNAME
+    if [ $hs = "server1" ]; then
+    echo "server2"
+    elif  [ $hs = "server2" ]; then
+    echo "server1"
+    else
+    echo "ERROR NOT RUNNINT ON SERVER"
+    fi
+}
+
+
+# 1) Check number of arguments is Valid
+echo "The number of arguments is: $#"
+if  (($# < 2)) ; then
+    echo "Illegal number of parameters"
+    exit
+fi
+
+# 2) Set the destantion server
+SERVER_DEST=$(set_destantion) 
+
+# 3) Get the destenation file and check if exciting: TRUE - Return , FALSE - Create the file
+DIR_DEST="${!#}"
+check_remote_dir $SERVER_DEST $DIR_DEST 
+sleep 1
+
+
+
+# 4) Check arguments are valide:
+        # Check that file exists: TRUE - copy the file, FALSE - Skip file 
+        # Sum the sizeof file
+echo "start loop:"
+SIZE=0
+for file in ${@:1:$#-1}; do
+
+        if [ -f "$file" ]; then
+            scp -qi  /home/vagrant/.ssh/id_rsa $file $SERVER_DEST:$DIR_DEST
+            SIZE=$((${SIZE}+$(du -b $file | awk '{ print $1 }')))
+        else
+            echo "$file: NOT A FILE! "
+        fi
+done
+
+echo $SIZE
